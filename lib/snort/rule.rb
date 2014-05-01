c6d93018eca46644beb08ee6eb0a4613863b3780
Will Green <will.green@mandiant.com>
2014-05-01 16:58:32 -0400

    Introduce Snort::RuleOption

--- lib/snort/rule.rb
+++ lib/snort/rule.rb
@@ -1,14 +1,31 @@
 require "snort/rule/version"
+require "snort/rule/option"
 # Generates and parses snort rules
 #
 # Author::    Chris Lee  (mailto:rubygems@chrislee.dhs.org)
 # Copyright:: Copyright (c) 2011 Chris Lee
 # License::   Distributes under the same terms as Ruby
 module Snort
+
 	# This class stores and generates the features of a snort rule
 	class Rule
-		attr_accessor :action, :proto, :src, :sport, :dir, :dst, :dport, :opts
+	  attr_accessor :action, :proto, :src, :sport, :dir, :dst, :dport, :opts
+    attr_reader :options
 
+    # Initializes the Rule
+    # @param [Hash] kwargs The options to initialize the Rule with
+    # @option kwargs [String] :action The action
+    # @option kwargs [String] :proto The protocol
+    # @option kwargs [String] :src The source IP
+    # @option kwargs [String] :sport The source Port
+    # @option kwargs [String] :dir The direction of traffic flow
+    # @option kwargs [String] :dst The destination IP
+    # @option kwargs [String] :dport The destination Port
+    # @option kwargs [Hash] :opts The (OLD AND BUSTED) way of passing in rule options.
+    #   This only works in very simple cases, and very poorly.
+    #   This is left here only for backwards compatibility. Please don't use this anymore.
+    # @option kwargs[Array<Snort::RuleOption>] :options The better way of passing in options, using
+    #   option objects that know how to represent themselves as a string properly
 		def initialize(kwargs={})
 			@action = kwargs[:action] || 'alert'
 			@proto = kwargs[:proto] || 'IP'
@@ -18,6 +35,7 @@ module Snort
 			@dst = kwargs[:dst] || 'any'
 			@dport = kwargs[:dport] || 'any'
 			@opts = kwargs[:opts] || {}
+      @options = kwargs[:options] || []
 		end
 
 		# Output the current object into a snort rule
@@ -25,7 +43,7 @@ module Snort
 			rule = ""
 			rule = [@action, @proto, @src, @sport, @dir, @dst, @dport].join(" ") unless options_only
 
-			if opts
+			if opts && opts.any?
   		  rule += " (" unless options_only
   			opts.keys.sort.each do |k|
 	  			rule += k if opts[k];
@@ -35,7 +53,13 @@ module Snort
 				  rule += "; "
 			  end
 			  rule += ")" unless options_only
-		  end
+      end
+
+      if options.any?
+        rule += " (" unless options_only
+        rule += options.join(' ')
+        rule += ")" unless options_only
+      end
 
 			rule
 		end
--- test/test_snort-rule.rb
+++ test/test_snort-rule.rb
@@ -24,7 +24,30 @@ class TestSnortRule < Minitest::Test
 		rule.opts['sid'] = 48
 		rule.opts['threshold'] = 'type limit,track by_src,count 1,seconds 3600'
 		assert_equal rule.to_s, "pass udp 192.168.0.1 any <> any 53 (sid:48; threshold:type limit,track by_src,count 1,seconds 3600; )"
-	end
+  end
+
+  def test_construct_a_default_rule_with_many_options_having_the_same_keyword
+    rule = Snort::Rule.new
+    rule.action = 'alert'
+    rule.proto = 'tcp'
+    rule.src = '$HOME_NET'
+    rule.dir = '->'
+    rule.dst = '$EXTERNAL_NET'
+    rule.dport = '$HTTP_PORTS'
+    rule.options << Snort::RuleOption.new('msg', '"HTTP Host www.baddomain.com"')
+    rule.options << Snort::RuleOption.new('content', '"Host|3a|"')
+    rule.options << Snort::RuleOption.new('nocase')
+    rule.options << Snort::RuleOption.new('http_header')
+    rule.options << Snort::RuleOption.new('content', '"www.baddomain.com"')
+    rule.options << Snort::RuleOption.new('nocase')
+    rule.options << Snort::RuleOption.new('http_header')
+    rule.options << Snort::RuleOption.new('pcre', '"/^Host\\x3a(.*\\.|\\s*)www\\.baddomain\\.com\\s*$/mi"')
+    rule.options << Snort::RuleOption.new('flow', 'to_server,established')
+    rule.options << Snort::RuleOption.new('threshold', 'type limit, track by_src, count 1, seconds 300')
+    rule.options << Snort::RuleOption.new('classtype', 'bad-unknown')
+    rule.options << Snort::RuleOption.new('sid', '100000000')
+    assert_equal 'alert tcp $HOME_NET any -> $EXTERNAL_NET $HTTP_PORTS (msg:"HTTP Host www.baddomain.com"; content:"Host|3a|"; nocase; http_header; content:"www.baddomain.com"; nocase; http_header; pcre:"/^Host\x3a(.*\.|\s*)www\.baddomain\.com\s*$/mi"; flow:to_server,established; threshold:type limit, track by_src, count 1, seconds 300; classtype:bad-unknown; sid:100000000;)', rule.to_s
+  end
 
 	def test_parse_an_existing_rule_and_generate_the_same_rule
 		rule = Snort::Rule.parse("pass udp 192.168.0.1 any <> any 53 (sid:48; threshold:type limit,track by_src,count 1,seconds 3600; )")
